86d011af3901923e0f4b55961cdcad93762cb821
wk-Nemo <2181544173@qq.com>
2021-03-12 20:52:51 +0800

    fix cros bug

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.all('*', function(req, res, next) {
+    　　//设置允许跨域的域名，*代表允许任意域名跨域
+        res.header("Access-Control-Allow-Origin", "*");
+    　　//允许的header类型
+        res.header('Access-Control-Allow-Headers', 'Content-type');
+    　　//跨域允许的请求方式
+        res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+    　　//可选，用来指定本次预检请求的有效期，单位为秒。在此期间，不用发出另一条预检请求。
+        res.header('Access-Control-Max-Age',1728000);//预请求缓存20天
+        next(); 
+});
+
 app.get('/index.json',(req,res) => {
     console.log(req);
     console.log(res);
